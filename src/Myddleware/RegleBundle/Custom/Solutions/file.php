6ba015c5a2e67c83313e1202b3835616d98f0e2b
Francesco Bianco <francescobianco@opencrmitalia.com>
2021-06-21 18:16:23 +0200

    add delete method on OCI

--- src/Myddleware/RegleBundle/Custom/Solutions/file.php
+++ src/Myddleware/RegleBundle/Custom/Solutions/file.php
@@ -4,18 +4,26 @@ namespace Myddleware\RegleBundle\Solutions;
 
 class filebase extends filecore
 {
+
     protected function get_last_file($directory, $date_ref)
     {
         $file = parent::get_last_file($directory, $date_ref);
 
         $fix = '[ -f zzz_fields ] && '
              . '[ "$(head -n1 zzz_fields)" != "$(head -n1 '.$file.')" ] && '
-             . 'printf "$(cat zzz_fields)\n$(cat '.$file.')\n" > '.$file;
+             . 'cat zzz_fields '.$file.' > '.$file.'.tmp && '
+             . 'mv '.$file.' '.$file.'.bak && '
+             . 'mv '.$file.'.tmp '.$file.' && '
+             . 'echo updated ||'
+             . 'echo ignored';
 
-        ssh2_exec($this->connection, 'cd '.$directory.'; '.$fix);
+        $stream = ssh2_exec($this->connection, 'cd '.$directory.'; '.$fix);
+        stream_set_blocking($stream, true);
+        $fixMessage = stream_get_contents($stream);
 
         return $file;
     }
+
 }
 
 $file = __DIR__.'/file.client.php';
