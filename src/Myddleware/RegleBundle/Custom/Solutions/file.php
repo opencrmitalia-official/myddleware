6ed230288b3a6ec54a0f856732cc5c1e9d67f6a2
Francesco Bianco <francescobianco@opencrmitalia.com>
2021-06-29 10:42:51 +0200

    minor fix

--- src/Myddleware/RegleBundle/Custom/Solutions/file.php
+++ src/Myddleware/RegleBundle/Custom/Solutions/file.php
@@ -7,16 +7,16 @@ class filebase extends filecore
 
     protected function get_last_file($directory, $date_ref)
     {
-        $file = parent::get_last_file($directory, $date_ref);
+        $file = trim(parent::get_last_file($directory, $date_ref));
 
         if ($file) {
             $fix = '[ -f zzz_fields ] && '
-                . '[ "$(head -n1 zzz_fields)" != "$(head -n1 '.$file.')" ] && '
-                . 'cat zzz_fields '.$file.' > '.$file.'.tmp && '
-                . 'mv '.$file.' '.$file.'.bak && '
-                . 'mv '.$file.'.tmp '.$file.' && '
-                . 'echo updated ||'
-                . 'echo ignored';
+                 . '[ "$(head -n1 zzz_fields)" != "$(head -n1 '.$file.')" ] && '
+                 . 'cat zzz_fields '.$file.' > '.$file.'.tmp && '
+                 . 'mv '.$file.' '.$file.'.bak && '
+                 . 'mv '.$file.'.tmp '.$file.' && '
+                 . 'echo updated || '
+                 . 'echo ignored';
 
             $stream = ssh2_exec($this->connection, 'cd '.$directory.'; '.$fix);
             stream_set_blocking($stream, true);
