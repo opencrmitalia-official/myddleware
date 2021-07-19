fb994421b76fcf16847984d50c3c45f921bd6559
Francesco Bianco <francescobianco@opencrmitalia.com>
2021-07-19 12:09:53 +0200

    minor fix

--- src/Myddleware/RegleBundle/Solutions/database.php
+++ src/Myddleware/RegleBundle/Solutions/database.php
@@ -417,7 +417,7 @@ class databasecore extends solution {
 				throw new \Exception('Read: '.$errorInfo[2].' . Query : '.$requestSQL);
 			}
 			$fetchAll = $q->fetchAll(\PDO::FETCH_ASSOC);
-
+            file_put_contents('/var/www/html/var/logs/fetchall.log', ">>> $requestSQL \n".json_encode($fetchAll)."\n", FILE_APPEND);
 			$row = array();
 			if(!empty($fetchAll)) {
 				$result['count'] = count($fetchAll);
