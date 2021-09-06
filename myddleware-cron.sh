906490d21f490eb3b9c90454acbd64b8c90b5e2f
Francesco Bianco <francescobianco@opencrmitalia.com>
2021-09-06 12:48:44 +0200

    minor fix

--- myddleware-cron.sh
+++ myddleware-cron.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
-set -e
 
+## Load variables
 source /run/crond.env
 
-echo $(date): Start Myddleware Sync >> /var/log/cron.log
-echo $(date): Start Myddleware Sync >> /var/log/myddleware.log
+## Prepare log file
+LOG_DIR=/var/www/html/var/logs/scheduler/$(date +"%Y/%m/%d")
+LOG_FILE=${LOG_DIR}/$(date +"%s").log
+mkdir -p ${LOG_DIR}
+touch ${LOG_FILE}
+chmod 777 -R ${LOG_DIR}
 
-php /var/www/html/bin/console myddleware:jobScheduler --env=background >> /var/log/myddleware.log
+## Execute jobs
+echo $(date): Start Myddleware Sync >> /var/log/cron.log
+echo $(date): Start Myddleware Sync >> ${LOG_FILE}
+
+php /var/www/html/bin/console myddleware:jobScheduler --env=background >> ${LOG_FILE}
 
 echo $(date): End Myddleware Sync >> /var/log/cron.log
-echo $(date): End Myddleware Sync >> /var/log/myddleware.log
+echo $(date): End Myddleware Sync >> ${LOG_FILE}
 
 ## Custom Scheduler
 if [[ -f /var/www/html/scheduler.sh ]]; then
