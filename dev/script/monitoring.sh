07d286d87b110903925c38c9417747a9063719d4
Francesco Bianco <francescobianco@opencrmitalia.com>
2021-09-07 17:15:15 +0200

    introducing monitoring

--- dev/script/monitoring.sh
+++ dev/script/monitoring.sh
@@ -6,6 +6,7 @@ source /run/crond.env
 ## Prepare log file
 LOG_DIR=/var/www/html/var/logs
 LOG_FILE=${LOG_DIR}/monitoring.log
+LOG_TIMESTAMP=$(date +"%Y-%m-%d %H:%M")
 touch ${LOG_FILE}
 chmod 777 ${LOG_FILE}
 
@@ -13,6 +14,6 @@ chmod 777 ${LOG_FILE}
 [ "$(wc -l < ${LOG_FILE})" -gt "1000" ] && sed -e '1,20d' -i ${LOG_FILE} || true
 
 ##
-echo "$(date) [INFO] Start monitoring..." >> ${LOG_FILE}
-php /var/www/html/bin/console myddleware:monitoring --env=background #>> ${LOG_FILE} 2>&1
-echo "$(date) [INFO] Done." >> ${LOG_FILE}
+echo "==> ${LOG_TIMESTAMP} [INFO] Start monitoring..." >> ${LOG_FILE}
+php /var/www/html/bin/console myddleware:monitoring --env=background >> ${LOG_FILE} 2>&1
+echo "==> ${LOG_TIMESTAMP} [INFO] Done." >> ${LOG_FILE}
