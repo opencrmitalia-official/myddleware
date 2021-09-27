52597e6535716763d2e25b3926fb188532d22b71
Francesco Bianco <francescobianco@opencrmitalia.com>
2021-09-27 15:54:35 +0200

    work in progress

--- src/Myddleware/RegleBundle/Command/monitoringCommand.php
+++ src/Myddleware/RegleBundle/Command/monitoringCommand.php
@@ -45,13 +45,31 @@ class monitoringCommand extends ContainerAwareCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $alertTimeLimit = 0;
+        $alertReminderTime = 0;
         $instanceName = 'Myddleware';
+        $notificationFile = $this->getContainer()->get('kernel')->getLogDir().'/notification.json';
+        $notificationStatus = [];
+        if (file_exists($notificationFile)) {
+            $notificationStatus = json_decode(file_get_contents($notificationFile), true);
+        }
+
         if (file_exists($customJsonFile = __DIR__.'/../Custom/Custom.json')) {
             $customJson = json_decode(file_get_contents($customJsonFile), true);
             $instanceName = isset($customJson['instance_name']) && $customJson['instance_name'] ? $customJson['instance_name'] : 'Myddleware';
             $alertTimeLimit = isset($customJson['alert_time_limit']) ? intval($customJson['alert_time_limit']) : 0;
+            $alertReminderTime = isset($customJson['alert_reminder_time']) ? intval($customJson['alert_reminder_time']) : 0;
         }
 
+        $this->alertNotification();
+
+        file_get_contents($notificationFile, json_encode($notificationStatus, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+	}
+
+    /**
+     *
+     */
+    protected function alertNotification()
+    {
         $db = $this->getContainer()->get('database_connection');
         $notification = $this->getContainer()->get('myddleware.notification');
 
@@ -63,12 +81,18 @@ class monitoringCommand extends ContainerAwareCommand
         if (count($jobs) > 0) {
             foreach ($jobs as $job) {
                 if ($alertTimeLimit > 0 && $job['busy_time'] > $alertTimeLimit) {
-                    echo "Sending alert notification for '{$instanceName}'...\n";
-                    $notification->sendAlert($alertTimeLimit);
-                    echo "Messages OK!\n";
-                    break;
+                    $waitForNotification = round($alertReminderTime - ((time() - @$notificationStatus['last_alert']) / 60));
+                    if ($waitForNotification < 0) {
+                        echo "Sending alert notification for '{$instanceName}'...\n";
+                        $notificationStatus['last_alert'] = time();
+                        $notification->sendAlert($alertTimeLimit);
+                        echo "Messages OK!\n";
+                        break;
+                    } else {
+                        echo "Postpone alert notification for '{$instanceName}' due to reminder until $waitForNotification minutes.\n";
+                    }
                 }
             }
         }
-	}
+    }
 }
