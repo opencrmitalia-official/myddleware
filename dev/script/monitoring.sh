bcc9aa124d4504091e58c1a47abe16a995335ffb
Francesco Bianco <info.francescobianco@gmail.com>
2021-09-29 12:11:46 +0200

    completed

--- dev/script/monitoring.sh
+++ dev/script/monitoring.sh
@@ -16,4 +16,4 @@ chmod 777 ${LOG_FILE}
 ##
 echo "==> ${LOG_TIMESTAMP} [INFO] Start monitoring..." >> ${LOG_FILE}
 php /var/www/html/bin/console myddleware:monitoring --env=background >> ${LOG_FILE} 2>&1
-echo "==> ${LOG_TIMESTAMP} [INFO] Done." >> ${LOG_FILE}
+echo "--" >> ${LOG_FILE}
