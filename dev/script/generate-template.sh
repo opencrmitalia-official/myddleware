f8ce0b8c3273b89d2f7eaae7ac494a1170820f84
Francesco Bianco <info.francescobianco@gmail.com>
2021-12-21 14:58:51 +0100

    add new tempalte

--- dev/script/generate-template.sh
+++ dev/script/generate-template.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-#php bin/console myddleware:generateTemplate "temp_template_gestionali" "Connessione con gestionali" "6188fa2ded232,6188fb2042051,6188fcc60dd12,6188fda3ef1a0,6188ffd300458,6189007e3636a,618901343fd8f,618a26ca31aca,618a42bcf1f58,618a48db15924,618a511ec0b54,618a516a909bc,618a51bfa20e5,618a52226c02f,618a7c75ce773,618a7e7fd235d" --env=background
 
+#php bin/console myddleware:generateTemplate "temp_template_gestionali" "Connessione con gestionali" "6188fa2ded232,6188fb2042051,6188fcc60dd12,6188fda3ef1a0,6188ffd300458,6189007e3636a,618901343fd8f,618a26ca31aca,618a42bcf1f58,618a48db15924,618a511ec0b54,618a516a909bc,618a51bfa20e5,618a52226c02f,618a7c75ce773,618a7e7fd235d" --env=background
 php bin/console myddleware:generateTemplate "microsoftsql_opencrmitalia_template_gestionali" "Connessione con gestionali" "6188fa2ded232,6188fb2042051,6188fcc60dd12,6188fda3ef1a0,6188ffd300458,6189007e3636a,618901343fd8f,618a26ca31aca,618a42bcf1f58,618a48db15924,618a511ec0b54,618a516a909bc,618a51bfa20e5,618a52226c02f,618a7c75ce773,618a7e7fd235d,61c1ad7622b91,61c1ae56e6e18" --env=background
