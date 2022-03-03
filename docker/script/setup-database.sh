d09037e067d019577d42220dc11a77aed3134c98
Francesco Bianco <info.francescobianco@gmail.com>
2022-03-03 14:48:56 +0100

    fix

--- docker/script/setup-database.sh
+++ docker/script/setup-database.sh
@@ -5,5 +5,5 @@ php bin/console doctrine:schema:update --force
 php bin/console doctrine:fixtures:load --append
 php bin/console myddleware:add-user admin secret docker@myddleware.com || true
 
-## Old code to promote to ROLE_SUPER_ADMIN
+## Promote admin user to ROLE_SUPER_ADMIN
 php bin/console myddleware:promote-user docker@myddleware.com ROLE_SUPER_ADMIN || true
