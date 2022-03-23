930d9e712729634abbfdd671d846ee31da3e6607
Francesco Bianco <info.francescobianco@gmail.com>
2022-03-23 11:58:20 +0100

    fix missing rename of vtiger custom

--- docker/script/reset.sh
+++ docker/script/reset.sh
@@ -5,7 +5,7 @@ echo "===> You are about to erase everything (type in capital letter: YES)?: "
 read AGREE
 
 if [ "${AGREE}" = "YES" ]; then
-  docker-compose -f docker/env/script.yml down -v --remove-orphans
+  touch .env.local && docker-compose down -v --remove-orphans && rm -f .env.local
   docker-compose -f docker/env/script.yml run --rm --no-deps myddleware rm -fr vendor node_modules
   docker-compose -f docker/env/script.yml run --rm --no-deps myddleware chmod -R 777 docker/tmp public/build public/js var/cache/prod docker/var/mysql
   mkdir -p docker/var/mysql && touch docker/var/mysql/.gitkeep
