e7fc765dda1a1877842a735c453d1374e226867c
francesco <info.francescobianco@gmail.com>
2022-10-20 15:36:19 +0200

    fix email from

--- src/Custom/Solutions/microsoftsqlcustom.php
+++ src/Custom/Solutions/microsoftsqlcustom.php
@@ -1,9 +1,25 @@
 <?php
+
 namespace App\Custom\Solutions;
 
 use App\Solutions\microsoftsql;
 
 class microsoftsqlcustom extends microsoftsql
 {
+    protected function generatePdo()
+    {
+        $newPdo = null;
 
+        $this->set_driver();
+        if ('sqlsrv' == $this->driver) {
+            $newPdo = new \PDO($this->driver . ':Server=' . $this->paramConnexion['host'] . ',' . $this->paramConnexion['port'] . ';Database=' . $this->paramConnexion['database_name'], $this->paramConnexion['login'], $this->paramConnexion['password']);
+        } else {
+            $newPdo = new \PDO($this->driver . ':host=' . $this->paramConnexion['host'] . ';port=' . $this->paramConnexion['port'] . ';dbname=' . $this->paramConnexion['database_name'] . ';charset=' . $this->charset, $this->paramConnexion['login'], $this->paramConnexion['password']);
+        }
+
+        $newPdo->query('SET ANSI_NULLS ON;');
+        $newPdo->query('SET ANSI_WARNINGS ON;');
+
+        return $newPdo;
+    }
 }
