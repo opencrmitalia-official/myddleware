61eef28af1dda43fb5f1bd2f4fc870d6b3907fdc
francesco <info.francescobianco@gmail.com>
2023-01-20 16:29:51 +0100

    fix development env

--- docker/script/myddleware-monitor.sh
+++ docker/script/myddleware-monitor.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+## Load variables
+source /run/crond.env
+export MYDDLEWARE_CRON_RUN=1
+
+## Run Monitoring Job
 php -f /var/www/html/bin/console myddleware:monitoring --env=background
