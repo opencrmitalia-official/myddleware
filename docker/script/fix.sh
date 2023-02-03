092861d13df5bfb8fb97bf87df7c3e813d048908
francescobianco <info.francescobianco@gmail.com>
2023-02-03 15:09:35 +0100

    fix

--- docker/script/fix.sh
+++ docker/script/fix.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+
+
 ## Fix permissions problem on `var/cache/` directory
 [ ! -d var/cache ] && mkdir -p var/cache
 chmod 777 -R var/cache
+
+[ ! -d var/cache/background ] && mkdir -p var/cache/background
+chmod 777 -R var/cache/background
 
 ## Fix permissions problem on `var/log/` directory
 [ ! -d var/log ] && mkdir -p var/log
