80e975cd215fe2d54833ecd374e1120da11ed709
francescobianco <info.francescobianco@gmail.com>
2023-02-03 16:00:19 +0100

    fix

--- src/Solutions/vtigercrm.php
+++ src/Solutions/vtigercrm.php
@@ -81,7 +81,7 @@ class vtigercrmcore extends solution
     ];
 
     // Module list that allows to make parent relationships
-    protected array $allowParentRelationship = ['Quotes', 'SalesOrder'];
+    protected array $allowParentRelationship = ['Quotes', 'SalesOrder', 'Invoice'];
 
     /** @var array */
     protected array $moduleList;
@@ -1354,12 +1354,24 @@ class vtigercrmcore extends solution
     {
         $log = json_encode($param, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
         file_put_contents('/var/www/html/var/log/itemsync.log', $log."\n", FILE_APPEND);
+
+        //$param['rule']['module_source'];
+
         if (isset($param['sourcePdo']) && is_object($param['sourcePdo'])) {
+
+/* source_view_output
+            NSERT INTO [dbo].[ITEMSYNC_ROWS_FEEDBACK]
+           ([]
+           ,[key_value]
+           ,[crmid]
+           ,[last_update])
+     VALUES*/
+
             $itemSyncFeedbackQuery = "
-                IF EXISTS(SELECT * FROM test WHERE id='')
-                    UPDATE test SET name='john' WHERE id=''
+                IF EXISTS(SELECT * FROM test WHERE source_view_output = '' AND key_value = '')
+                    UPDATE ITEMSYNC_ROWS_FEEDBACK SET name='john' WHERE id=''
                 ELSE
-                    INSERT INTO test(name) VALUES ('john');
+                    INSERT INTO ITEMSYNC_ROWS_FEEDBACK (name) VALUES ('john');
             ";
             $param['sourcePdo']->prepare($itemSyncFeedbackQuery)->execute();
         }
